e6255c4ee0e376139646f4be2453db7c2c4e691a
thekushalghosh <kushalghosh08@gmail.com>
2020-08-25 10:04:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside
+//of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -59,7 +60,6 @@ windows.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
